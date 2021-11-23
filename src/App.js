02a18a416ef9b8c018c14cdc6348a0e1eb6ade28
AndreyMandybura <80692584+AndreyMandybura@users.noreply.github.com>
2021-11-23 18:43:50 +0200

    Inintial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Initialization</h1>
+        
+      </div>
+      );
+  }
+}
+
+export default App;
